c2803835c6b840db21d385655bb893aa04546cda
João Gabryel <126635450+JoaoGabryel@users.noreply.github.com>
2024-10-04 23:22:13 -0300

    Refatoração do Projeto Back-End

--- projetoCasaDaPaz/routes/web.php
+++ projetoCasaDaPaz/routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
-use App\Http\Controllers\ClientController;
-use App\Http\Controllers\PremioController;
-use App\Http\Controllers\SiteController;
-use App\Http\Controllers\ProdutoController;
-use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-/*
-Route::get('/premios',[PremioController::class,'index'])->name('premios.index');
-
-Route::get('/premios/{id}',[PremioController::class,'show'])->name('premios.show');
-*/
-Route::post('/login', [UsuarioController::class],'show');
