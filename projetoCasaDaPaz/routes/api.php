be8d1415e04145089f2e70cedad56d83f82a469a
João Gabryel <126635450+JoaoGabryel@users.noreply.github.com>
2024-11-13 00:12:33 -0300

    reintegração das rotas de usuarios

--- projetoCasaDaPaz/routes/api.php
+++ projetoCasaDaPaz/routes/api.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FotoController;
 use App\Http\Controllers\GaleriaController;
@@ -14,6 +15,7 @@ use App\Http\Controllers\HistoriaController;
 use App\Http\Controllers\ImagemController;
 use App\Http\Controllers\ParceiroController;
 use App\Http\Controllers\RedeSocialController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, 'register']);
@@ -123,3 +125,8 @@ Route::middleware('auth:api')->group(function () {
     Route::put('/parceiros/{id}', [ParceiroController::class, 'update']);
     Route::delete('/parceiros/{id}', [ParceiroController::class, 'destroy']);
 });
+Route::get('/usuarios', [UsuarioController::class, 'index']);
+Route::post('/usuarios', [UsuarioController::class, 'store']);
+Route::get('/usuarios/{id}', [UsuarioController::class, 'show']);
+Route::put('/usuarios/{id}', [UsuarioController::class, 'update']);
+Route::delete('/usuarios/{id}', [UsuarioController::class, 'destroy']);
